232f328f14d1d360c1ca0bc1865d78e0e809a036
Volker Veidt <vmx17@gmx.com>
2022-09-09 17:57:06 +0900

    uncompleted renaming

---
+++ RenderersWRC/pch.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+
+#include <wrl.h>
+#include <wrl/client.h>
+#include <dxgi1_4.h>
+#include <d3d11_3.h>
+#include <d2d1_3.h>
+#include <d2d1effects_2.h>
+#include <dwrite_3.h>
+#include <wincodec.h>
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <memory>
+//#include <agile.h>
+#include <concrt.h>
+//#include <collection.h>
+
+
+#include <unknwn.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+// for xaml_typename what WinUI3 template does not have.
+#include <winrt/Windows.UI.Xaml.Interop.h>
+
+#include <winrt/Microsoft.UI.Composition.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Data.h>
+#include <winrt/Microsoft.UI.Xaml.Markup.h>
+#include <winrt/Microsoft.UI.Xaml.Navigation.h>
+
+#include <winrt/Windows.System.Threading.h>
+#include <winrt/Windows.Graphics.Display.h>
+#include <winrt/Windows.UI.Core.h>
+//#include "BoxRenderer.h"
+
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+typedef long long int64;
